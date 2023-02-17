0ddd3cb9fc139d613290855458d408a517ee2cfc
okusindja <okusindja@gmail.com>
2023-02-17 03:41:00 +0100

    :rocket: chore: finalização

--- projecto/src/componentes/HomePage.jsx
+++ projecto/src/componentes/HomePage.jsx
@@ -1,18 +1,60 @@
 import Menu from "./Menu.jsx";
+import "./homePage.css";
 import BarraInformativa from "./BarraInformativa.jsx";
+import { useEffect, useState } from "react";
 
-function HomePage()
-{
-    return(
-        <div>
-        
+function HomePage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://192.168.1.2:3001/users");
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`http://192.168.1.2:3001/users/${id}`, { method: "DELETE" })
+      .then(() => setData(data.filter((user) => user.id !== id)))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <>
+      <div>
         <Menu nomeUser="Luís Alexandre"></Menu>
-
-        <BarraInformativa Saldo1="15.000kz" Saldo2="20.000kz" Saldo3="30.000kz"></BarraInformativa>
-
-        
-        </div>
-    );
+        <BarraInformativa
+          Saldo1="15.000kz"
+          Saldo2="20.000kz"
+          Saldo3="30.000kz"
+        ></BarraInformativa>
+      </div>
+      <div className="user-table">
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Endereço</th>
+            <th>Password</th>
+            <th>Deletar</th>
+          </tr>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>{item.endereco}</td>
+              <td>**************</td>
+              <td>
+                <button onClick={() => handleDelete(item.id)}>Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
--- projecto/src/componentes/LadoEsquerdo.jsx
+++ projecto/src/componentes/LadoEsquerdo.jsx
@@ -1,36 +1,73 @@
+import { useEffect, useState } from "react";
 import "../img/logo.png";
 import "./LadoEsquerdo.css";
 import { Link } from "react-router-dom";
 
 function LadoEsquerdo(props) {
+  // const [data, setData] = useState([]);
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch("http://192.168.1.2:3001/users");
+  //     const data = await response.json();
+  //     setData(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
+  // const handleLogin = () => {
+  //   const user = data.find(
+  //     (u) => u.username === username && u.password === password
+  //   );
+  //   if (user) {
+  //     alert("Login feito!");
+  //   } else {
+  //     alert("Nome ou senha incorretos");
+  //   }
+  // };
   return (
     <div className="ladoEsquerdo">
-
       <div id="Header">
-      <Link  to={"/HomePage"}>  <h3 id="Visitante">{props.Visitante}</h3> </Link>
-      <Link  to={"/TelaCadastro"}>   <h3 id="SignUp">{props.SignUp}</h3> </Link>
-           <h3 id="Login">{props.Login}</h3> 
+        <Link to={"/HomePage"}>
+          {" "}
+          <h3 id="Visitante">{props.Visitante}</h3>{" "}
+        </Link>
+        <Link to={"/TelaCadastro"}>
+          {" "}
+          <h3 id="SignUp">{props.SignUp}</h3>{" "}
+        </Link>
+        <h3 id="Login">{props.Login}</h3>
       </div>
-       
-       
-
-      
-       
-       <div className="corpo">
-              <img src={require('../img/logo.png')} alt="" className="image"></img>
-
-              <h3 id="LoginE">{props.LoginE}</h3>
-
-              <input type="text" id="Email" placeholder="Email"/>
-              
-              <input type="text" id="Pass" placeholder="palavra-passe"/> 
-              
-              <Link  to={"/HomePage"}>   <input type="button" value="Entrar" id="botao"  /> </Link>
-              
-              <h4 id="Link">{props.Link}</h4>
-       </div>
-       
-       
+      <div className="corpo">
+        <img src={require("../img/logo.png")} alt="" className="image"></img>
+        <h3 id="LoginE">{props.LoginE}</h3>
+        <input
+          type="text"
+          id="Email"
+          placeholder="Nome de usuário"
+          // value={username}
+          // onChange={setUsername}
+        />
+        <input
+          type="text"
+          id="Pass"
+          placeholder="palavra-passe"
+          // value={password}
+          // onChange={setPassword}
+        />
+        <Link to={"/HomePage"}>
+          {" "}
+          <input
+            type="button"
+            value="Entrar"
+            id="botao"
+            // onSubmit={handleLogin}
+          />{" "}
+        </Link>
+        <h4 id="Link">{props.Link}</h4>
+      </div>
     </div>
   );
 }
